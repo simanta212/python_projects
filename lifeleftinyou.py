3e040ab9e6cde16b6bb92b6a6acafb0705cc8d77
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-16 10:03:21 +0545

    used "f-string" and mathematical operators
    #100days coding challenge
    - Day 2

--- lifeleftinyou.py
+++ lifeleftinyou.py
@@ -1,11 +1,18 @@
 user_age    =   int(input())
 #user_age_conerter = int(user_age)
 
-x = 90 - user_age #age left in years
-y = x * 365       #age left in days
-z = y / 30        #age left in months
-a = y / 52        #age left in weeks   
-    
-print(f"age in years left {x},\nage in days left {y},\nage in months left {z},\nage in weeks left {a}")
 
+if user_age <= 90:
+    x = 90 - user_age #age left in years
+    y = x * 365       #age left in days
+    z = y / 30        #age left in months
+    a = y / 52        #age left in weeks   
+    print(f"age in years left {x},\nage in days left {y},\nage in months left {z},\nage in weeks left {a}")
+
+else:
+    print("Aba dherai time xaina")
+
+
+
+    
 
