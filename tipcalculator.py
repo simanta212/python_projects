f046a66d58f6bbb2d35e07460bd7f1f122bcda47
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-16 09:35:02 +0545

    Update tipcalculator.py

--- tipcalculator.py
+++ tipcalculator.py
@@ -7,7 +7,9 @@ User_split_peoples = int(input())
 
 Bill_Percent = User_tip_percentage * .1
 Bill_With_Tip = Bill_Percent + User_bill
-Final_Bill = Bill_With_Tip / User_split_peoples
+Total_Bill_Per_person = Bill_With_Tip / User_split_peoples
+Final_Bill = round(Total_Bill_Per_person, 2)
+
 
 print(f"Total Bill for {User_split_peoples} peoples is {Final_Bill} each")
 
