39cacabae27f4bba2832f91aa696a5b77dd8a72d
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-17 09:58:13 +0545

    leap year calculator
    #100days coding challenge
    - Day 3

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,6 @@
 year = int(input("what year is it\n"))
 
-if year/4==0 or year/100==0 or year/400:
+if year/4==0 or year/100==0 or year/400==0:
     print(f"yea, year {year} is leap year")
 else:
     print(f"No {year} is not leap year")
