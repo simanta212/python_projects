a9b4438678ee79aba0ebff1eb54606f1d54996f1
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-18 10:40:31 +0545

    fixed operator error
    #100days coding challenge
    - Day 2
    ---------What's new ?-----------
    used .lower & .count functions

--- tipcalculator.py
+++ tipcalculator.py
@@ -5,7 +5,7 @@ User_tip_percentage = float(input())
 print("Number of peoples to split the bill with")
 User_split_peoples = int(input())
 
-Bill_Percent = User_tip_percentage * .1
+Bill_Percent = User_tip_percentage * 1
 Bill_With_Tip = Bill_Percent + User_bill
 Total_Bill_Per_person = Bill_With_Tip / User_split_peoples
 Final_Bill = round(Total_Bill_Per_person, 2)
