ca787efa566eaf4c5def8ab0d087804c34591d4b
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-20 10:52:45 +0545

    forloop in action
    #100days coding challenge
    - Day 5
    ---------What's new ?-----------
    used forloop to obtain max score from the list

---
+++ highscorer.py
@@ -0,0 +1,24 @@
+score = [48, 55, 66, 78, 90, 66, 72]
+
+students = 0
+tot = 0
+top_scorer = 0
+# max
+
+# for s in score:
+#     students += 1
+
+# for total in score:
+#     tot += total
+
+for max in score:
+    if  max > top_scorer:
+        top_scorer = max
+
+
+
+print(top_scorer)
+# print(tot)
+
+# print(f"Total students participated in exam are {students}")
+
