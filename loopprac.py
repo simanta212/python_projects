fe3f1101ed4dfb33890cecad05cd135966522e09
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-11-20 15:46:47 +0545

    playing with for-loop
    #100days coding challenge
    - Day 6
    ---------What's new ?-----------
    used for-loop

---
+++ loopprac.py
@@ -0,0 +1,13 @@
+n = int(input())
+def loopgod(n):
+    for number in range (n, n*n+n, n ):
+        print (number)
+            
+print(loopgod(n))
+
+# print the number that is n*n and should display each iteration/step.
+# input = 10 
+# output = 10 20 30 40 50 60 70 80 90 100
+
+
+# problem - showing None at the end of the execution. (status: not solved) 
