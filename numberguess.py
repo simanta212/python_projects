efade634200991031a80af8db5bc2102147ac199
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-12-08 09:13:07 +0545

    puzzle game
    guess the number from 1-50.
    User_tries: 5-10

--- numberguess.py
+++ numberguess.py
@@ -1,7 +1,7 @@
 from math import gamma
 import random
 
-NUM_LIST = list(range(1,20))
+NUM_LIST = list(range(1,50))
 
 number_picker = random.choice(NUM_LIST)
 
@@ -11,28 +11,30 @@ number_picker = random.choice(NUM_LIST)
 
 game_type = input("Type 'easy' for easy and 'hard' for hard mode\n").lower()
 #life = 0   
-
-
-def play_game():
-    if game_type == "easy":
+if game_type == "easy":
         life = 10
-    if  game_type == "hard":
+if  game_type == "hard":
         life = 5
+game_finished = False
+
+while not game_finished :
     User_guess = int(input("Guess the number\n"))
-    life -= 1
-    print(f"Your have {life} try left")
-    #if number_picker < 10 and User_guess > 10:
+    
+    
+        #if number_picker < 10 and User_guess > 10:
     if number_picker > User_guess:
         print("Too low") 
         life -= 1
-    #elif number_picker > 10 and number_picker < 20 and User_guess < 10 and User_guess > 20:
+        #elif number_picker > 10 and number_picker < 20 and User_guess < 10 and User_guess > 20:
     elif number_picker < User_guess:
         print("Too high")
         life -= 1
-    if User_guess == number_picker:
+    elif User_guess == number_picker:
         print("You guess the correct number") 
     
-while game_type != 0:    
-    play_game()
+    print(f"Your have {life} try left")
+    
+    if life == 0:
+        game_finished = True
+        print("retry")
 
-play_game()
