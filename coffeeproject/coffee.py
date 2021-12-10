4cc968e81ec02cacabf1099614c0222107c2c0e7
simanta212 <48249158+simanta212@users.noreply.github.com>
2021-12-10 13:51:50 +0545

    coffee machine
    building logic of coffee machine

--- coffeeproject/coffee.py
+++ coffeeproject/coffee.py
@@ -7,21 +7,30 @@ Items_cappuccino = Items["cappuccino"]
 # print(Items["espresso"])
 
 
-order_false = True
-while order_false:
-    def rerun():
+
+def rerun():
+    order_false = True
+    while order_false:
         user_choice = input("what do you want? \n'espresso','latte', 'cappuccino'?").lower()
+        if user_choice == "off":
+            print("out os service")
+            exit()
         if user_choice == "espresso":
             print(Items_espresso["cost"])
             print("dollars")
+            rerun()
         elif user_choice == "latte":
             print(Items_latte["cost"])   
             print("dollars")
+            rerun()
         elif user_choice == "cappuccino":
             print(Items_cappuccino["cost"])
             print("dollars")
+            rerun()
+        
         else: 
             
             print ("please type correct name of the product")
             order_false = False
-    rerun()
+            rerun()
+rerun()     
